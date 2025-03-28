d31c597ab27383907dbd1c1a543a6dde2f841564
Sonu17-05 <vcoffsetprinters7@gmail.com>
2025-03-28 14:50:02 +0530

    Create String_.java

---
+++ String_.java
@@ -0,0 +1,13 @@
+public class String_ {
+    public static void main(String[] args) {
+        String str="Java Programming";
+        System.out.println("The string is '"+str+"'");
+        System.out.println("Length of the String is "+str.length());
+        System.out.println("Character at second and fourth position is "+str.charAt(1)+","+str.charAt(3));
+        System.out.println("The sub string using start index only is "+str.substring(5));
+        System.out.println("The sub string using start index only is "+str.substring(0,4) );
+
+
+    }
+    
+}
