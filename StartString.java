eafb0dc6b7953aea887757a6f1d9a2eed07ae1e1
Sonu17-05 <vcoffsetprinters7@gmail.com>
2025-03-28 14:49:20 +0530

    Create StartString.java

---
+++ StartString.java
@@ -0,0 +1,8 @@
+public class StartString {
+    public static void main(String[] args) {
+        String s1 ="Good Morning";
+        System.out.println(s1);
+        System.out.println("To Upper case:"+s1.toUpperCase());
+        System.out.println("To Lower caes:"+s1.toLowerCase());
+    }
+}
