cfd9e36ded842c83f287010c1f5223de0d4bafeb
Sonu17-05 <vcoffsetprinters7@gmail.com>
2025-03-28 14:46:28 +0530

    Create MatrixAddition.java

---
+++ MatrixAddition.java
@@ -0,0 +1,35 @@
+public class MatrixAddition {
+    public static void main(String[] args) {
+        // Example matrices
+        int[][] matrix1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        int[][] matrix2 = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+        
+        // Resultant matrix to store the sum
+        int[][] result = new int[3][3];
+        
+        // Adding matrices
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        
+        // Display the result
+        System.out.println("Result of Matrix Addition:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
